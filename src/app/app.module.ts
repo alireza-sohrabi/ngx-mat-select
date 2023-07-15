36c07b91653b270acdedcc6f17156aa6808069e3
ar.mohammadi@mahsan.co <ar.mohammadi@mahsan.co>
2023-07-15 15:20:56 +0330

    version 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NGX_MAT_SELECT_CONFIG, NgxMatSelectConfig} from './ngx-mat-select';
+import {NGX_MAT_SELECT_CONFIG, NgxMatSelectConfig} from './ngx-mat-select/pubilc_api';
 import {ExamplesModule} from "./examples/examples.module";
 
 export const ngxMatSelectConfigs: NgxMatSelectConfig = {}
