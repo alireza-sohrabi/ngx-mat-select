6e42cda78f45ee00bc3788a63012e4b90cf170e6
Alireza Sohrabi <alireza@limespot.com>
2026-08-05 11:07:25 +0300

    Restore Angular 16 baseline (#58)

--- src/app/ngx-mat-select/component/panel/ngx-mat-select-panel.module.ts
+++ src/app/ngx-mat-select/component/panel/ngx-mat-select-panel.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {CdkConnectedOverlay} from '@angular/cdk/overlay';
+import {CdkConnectedOverlay, OverlayModule} from '@angular/cdk/overlay';
 
 import {NgxMatSelectPanelComponent} from './ngx-mat-select-panel.component';
 
 @NgModule({
   declarations: [NgxMatSelectPanelComponent],
-  imports: [CommonModule, CdkConnectedOverlay],
+  imports: [CommonModule, OverlayModule, CdkConnectedOverlay],
   exports: [NgxMatSelectPanelComponent],
 })
 export class NgxMatSelectPanelModule {}
